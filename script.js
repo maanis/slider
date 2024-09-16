19077aedbd253e00b5643f6bb75de0685c6ef79f
Manish Jha <manishjha962@gmail.com>
2024-09-16 06:46:21 -0700

    done

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
 let right = document.querySelector('.right')
 let left = document.querySelector('.left')
 
-let item = document.querySelectorAll('.item')
-var check = 0
+
 right.addEventListener('click', ()=>{
-if(check === 0){
+    let item = document.querySelectorAll('.item')
     document.querySelector('.slider').appendChild(item[0])
-    check = 1
-}
-if(check === 1){
-    document.querySelector('.slider').appendChild(item[1])
-    check = 2
-}
-if(check === 2){
-    document.querySelector('.slider').appendChild(item[2])
-    check = 3
-}
+    
 })
 
 left.addEventListener('click',()=>{
+    let item = document.querySelectorAll('.item')
     document.querySelector('.slider').prepend(item[item.length - 1])
 })
