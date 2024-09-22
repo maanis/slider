aa8939bc5642f7dd344a22307b106dd95dac42b0
Manish Jha <manishjha962@gmail.com>
2024-09-21 22:14:55 -0700

    dpne slider

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 let right = document.querySelector('.right')
 let left = document.querySelector('.left')
+document.body.style.backgroundImage
+let slider = document.querySelector('.slider')
+let tl = gsap.timeline()
 
-
-right.addEventListener('click', ()=>{
+console.log(slider.style.background.src)
+var check = 1
+right.addEventListener('click', () => {
     let item = document.querySelectorAll('.item')
     document.querySelector('.slider').appendChild(item[0])
-    
+    console.log(item[2].style.backgroundImage)
+    document.querySelector('.hero-img').style.background = `${item[2].style.backgroundImage} no-repeat center center/cover`
+    gsap.from('.head',{
+        y:25
+    })
 })
 
-left.addEventListener('click',()=>{
+left.addEventListener('click', () => {
     let item = document.querySelectorAll('.item')
     document.querySelector('.slider').prepend(item[item.length - 1])
+    document.querySelector('.hero-img').style.background = `${item[0].style.backgroundImage} no-repeat center center/cover`
 })
