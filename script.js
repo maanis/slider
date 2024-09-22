1ac8d987692915ea9fd63438f9213f612fea0968
Manish Jha <manishjha962@gmail.com>
2024-09-22 10:51:19 +0530

    gsap

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let right = document.querySelector('.right')
 let left = document.querySelector('.left')
 document.body.style.backgroundImage
 let slider = document.querySelector('.slider')
-let tl = gsap.timeline()
+
 
 console.log(slider.style.background.src)
 var check = 1
@@ -11,9 +11,6 @@ right.addEventListener('click', () => {
     document.querySelector('.slider').appendChild(item[0])
     console.log(item[2].style.backgroundImage)
     document.querySelector('.hero-img').style.background = `${item[2].style.backgroundImage} no-repeat center center/cover`
-    gsap.from('.head',{
-        y:25
-    })
 })
 
 left.addEventListener('click', () => {
