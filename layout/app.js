56dc74854153f8d93502c1a7d6ec4543aad65b2c
Manish Jha <manishjha962@gmail.com>
2024-09-26 22:40:21 +0530

    done layout

---
+++ layout/app.js
@@ -0,0 +1,95 @@
+var tl = gsap.timeline()
+gsap.from('.boxB',{
+    duration:1.7,
+    x:-200,
+    // width:"15%",
+    y:100,
+    delay:1,
+    scale:1.2
+})
+gsap.from('.nav',{
+    scale:0,
+    opacity:0,
+    // duration:2,
+    delay:2.3,
+    x:165,
+    y:50
+})
+gsap.from('.boxA',{
+    scale:0,
+    opacity:0,
+    x:320,
+    y:145,
+    delay:2.3,
+})
+gsap.from('.boxC',{
+    scale:0,
+    opacity:0,
+    x:255,
+    y:-115,
+    delay:2.3,
+})
+gsap.from('.boxD',{
+    scale:0,
+    opacity:0,
+    x:122,
+    y:-110,
+    delay:2.3,
+})
+gsap.from('.boxE',{
+    scale:0,
+    opacity:0,
+    x:-200,
+    y:-200,
+    delay:2.3,
+})
+gsap.from('.boxF',{
+    scale:0,
+    opacity:0,
+    x:-165,
+    y:-68,
+    delay:2.3,
+})
+
+gsap.from('.logo',{
+    opacity:0,
+    x:-45,
+    delay:3
+})
+gsap.from('li',{
+    opacity:0,
+    y:45,
+    delay:3,
+    stagger:.2
+})
+gsap.from('.boxA-text',{
+    y:45,
+    opacity:0,
+    delay:3,
+    duration:1
+})
+gsap.from('i',{
+    scale:0,
+    opacity:0,
+    delay:3.4
+})
+gsap.from('.boxc-text',{
+    opacity:0,
+    y:95,
+    delay:3,
+    duration:1,
+    stagger:.6
+})
+
+gsap.from('.img',{
+    scale:0,
+    opacity:0,
+    delay:3
+})
+gsap.from('input',{
+    y:20,
+    opacity:0,
+    delay:3.4,
+    duration:1,
+    stagger:.4
+})
